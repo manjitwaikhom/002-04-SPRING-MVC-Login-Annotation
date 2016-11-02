c54e2de09a608343383e55018e3414733be03e06
manjitwaikhom <manjitwaikhom@gmail.com>
2016-11-02 11:46:32 +0530

    logs added

--- 002-04-SPRING MVC Login (Annotation)/src/main/java/com/spring/mvc/LoginController.java
+++ 002-04-SPRING MVC Login (Annotation)/src/main/java/com/spring/mvc/LoginController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import javax.servlet.ServletException;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -15,13 +16,14 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 @Controller
 @SessionAttributes
 public class LoginController {
-
+	final static Logger LOGGER = Logger.getLogger(LoginController.class);
 	@Autowired
 	private UserValidator userValidator;
 
 	@RequestMapping(value = "/verifyUser", method = RequestMethod.POST)
 	public String verifyUser(@ModelAttribute("user") User user,
 			BindingResult result) {
+		LOGGER.debug("verifyUser() method called");
 		System.out.println("inside verifyUser() method");
 		userValidator.validate(user, result);
 		if (result.hasErrors()) {
@@ -34,6 +36,7 @@ public class LoginController {
 		String pw = user.getPassword();
 		System.out.println(un);
 		System.out.println(pw);
+		LOGGER.info("username: " + un + "  Password: " + pw);
 		if (!un.equals(pw)) {
 			return "login";
 		}
@@ -42,6 +45,7 @@ public class LoginController {
 
 	@RequestMapping(value = "/login")
 	public String showLoginForm(Map model) throws ServletException {
+		LOGGER.debug("showLoginForm() method called");
 		System.out.println("inside showLoginForm() method");
 		User user = new User();
 		user.setUsername("manjit");
--- 002-04-SPRING MVC Login (Annotation)/src/main/java/com/spring/mvc/UserValidator.java
+++ 002-04-SPRING MVC Login (Annotation)/src/main/java/com/spring/mvc/UserValidator.java
@@ -1,24 +1,30 @@
 package com.spring.mvc;
 
+import org.apache.log4j.Logger;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
-public class UserValidator implements Validator{
+public class UserValidator implements Validator {
+	final static Logger LOGGER = Logger.getLogger(UserValidator.class);
 
-	public boolean supports(Class clazz){
+	public boolean supports(Class clazz) {
 		return User.class.equals(clazz);
 	}
-	
-	public void validate(Object command,Errors errors){
-		User user=(User)command;
-		if(user.getUsername()==null||user.getUsername().length()==0){
-			errors.rejectValue("username", "errors.username.required",null,"Username Required");
-			
+
+	public void validate(Object command, Errors errors) {
+		User user = (User) command;
+		if (user.getUsername() == null || user.getUsername().length() == 0) {
+			LOGGER.debug("Username Required");
+			errors.rejectValue("username", "errors.username.required", null,
+					"Username Required");
+
 		}
-		
-		if(user.getPassword()==null||user.getPassword().length()==0){
-			errors.rejectValue("password", "errors.password.required",null,"Password Required");
-			
+
+		if (user.getPassword() == null || user.getPassword().length() == 0) {
+			LOGGER.debug("Password Required");
+			errors.rejectValue("password", "errors.password.required", null,
+					"Password Required");
+
 		}
 	}
 }
